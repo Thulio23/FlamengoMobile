096ec5c56e2a8cd55a825edb3a5ad99000963eac
THULIO23 <THULIOVICTOR23@GMAIL.COM>
2024-03-14 13:15:02 -0300

    flamengo

---
+++ src/screens/Slide1/index.tsx
@@ -0,0 +1,47 @@
+import { Text, View, Image, TouchableHighlight } from "react-native";
+import { ImgDesc } from "../../components/ImgDesc";
+import { colors } from "../../styles/colors";
+import { styles } from "../../styles/GlobalStyle";
+interface troca{
+    altera: React.Dispatch<React.SetStateAction<number>>
+
+}
+export function Slide1({altera}:troca) {
+    const Images = [
+        {
+            url: require('../../assets/1.png'),
+            desc: "8x Brasileiro"
+        },
+        {
+            url: require('../../assets/2.png'),
+            desc: "4x Copa do Brasil"
+        },
+        {
+            url: require('../../assets/3.png'),
+            desc: "3x Libertadores"
+        },
+        {
+            url: require('../../assets/4.png'),
+            desc: "1x Mundial"
+        }
+
+    ]
+    return (
+        <View>
+            
+             <Text style={styles.pageTitle}>Bem Vindo ao Aplicativo do  </Text>
+             <TouchableHighlight onPress={() => {
+                altera(2)
+            }}>
+                <Text style={styles.pageTitle}>Flamengo</Text>
+            </TouchableHighlight>
+             <View>
+            {Images.map(json => (
+                <ImgDesc url={json.url} desc={json.desc}/>         
+            ))}
+            </View>
+
+        </View>
+    )
+
+}
---
+++ src/screens/index.tsx
@@ -0,0 +1,3 @@
+import { Slide1 } from "./Slide1";
+import { Slide2 } from "./Slide2";
+import { Slide3 } from "./Slide3";
